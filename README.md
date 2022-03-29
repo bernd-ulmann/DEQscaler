d8cc4c03c9d7793e67b12833504227e1fbcca1a8
WStefani <wot@WSta>
2022-03-29 11:18:16 +0200

    Initial Code Commit

--- README.md
+++ README.md
@@ -7,11 +7,203 @@ denoting the "machine units" of the analog computer).
 
 # Usage example
 
-...
+First import the DEQscaler class (ensure the correct working directory for the relative import) and sympy:
 
-# Implementation
+```python
+from source.DEQscaler import DEQscaler
+import sympy
+```
 
-...
+We use the Roessler System
+
+dx/dt = -y - z
+
+dy/dt =  x + a*y
+
+dz/dt =  b + z*(x-c) = b - c*z + x*z
+
+with initial values y0 = (1, 0, 0) and parameters (a, b, c) = (.2, .2, 5.7) as an example for rescaling here.
+
+
+Define the starting value y(t0) = y0 and the time frame for t, here the interval (0,25).
+```python
+y0 = [1, 0, 0]
+t_span = (0.0, 25.0) # = (t0, tf)
+```
+
+Aggregate them as arguments for solve_ivp; the fun argument for solve_ivp will be created automatically from the sympy equations.
+```python
+args_solve_ivp_ini_val = [t_span, y0]
+```
+
+Throughout the following be aware of the difference between a (Python) variable, such as "a" or "y", and the object saved therein, such as sympy.Symbol('a') or sympy.Symbol('y'). 
+
+Define the parameters used in the equation and provide their values in a dictionary.
+```python
+a, b, c =  sympy.symbols('a b c')
+diff_eq_parameters = {a : .2, b : .2, c : 5.7}
+```
+
+Define the variables involved and use them to create the sympy represenation of the system of differential equations.
+```python
+t, x, y, z = sympy.symbols('t x y z')
+dydt = [x, y, z]
+f_t_y = [-y-z, x+a*y, b+z*(x-c)]
+```
+
+Use these to create the DEQscaler instance.
+```python
+Roessler = DEQscaler(args_solve_ivp_ini_val, t, dydt, f_t_y, diff_eq_parameters)
+```
+
+First verify all entries are as intended. [Note the x, y, z, a, b, c are all sympy.Symbol().]
+```python
+Roessler.show_eqn()
+```
+
+To obtain a list which can be used to create a rescaled equation, the .create_rescaled_Diff_Eq() can be used.
+This may take some time, since the numerical integration is then performed.
+```python
+properties_rescaled = Roessler.create_rescaled_Diff_Eq()
+```
+
+Using these a rescaled Roessler equation can be created.
+```python
+Roessler_rescaled = DEQscaler(*properties_rescaled)
+Roessler_rescaled.show_eqn()
+```
+
+To verify the new (absolut) maxima these can be calculated and printed.
+```python
+Roessler_rescaled.determine_max()
+print(Roessler_rescaled.maxima)
+```
+
+This may yield unsatisfactory (cf. values >= 1) results such as:
+```
+{x: 1.000052590888878, y: 0.9991044611593582, z: 0.9997609510430012}
+```
+
+The calculated (absolut) maxima of the initial Roessler System are:
+```python
+print(Roessler.maxima)
+```
+
+```
+{x: 10.435175428044776, y: 9.584035371572032, z: 2.3440549911816726}
+```
+
+When rescaling one could use increased values of these by defining a max_scale_factor, such as an addition of 1%:
+```python
+Roessler_2 = DEQscaler(args_solve_ivp_ini_val, t, dydt, f_t_y, diff_eq_parameters, max_scale_factor=1.01)
+Roessler_rescaled_2 = DEQscaler(*Roessler_2.create_rescaled_Diff_Eq())
+Roessler_rescaled_2.determine_max()
+```
+
+Now the maxima of Rossler and Roessler_2 are still the same:
+```python
+print(Roessler.maxima)
+print(Roessler_2.maxima)
+```
+
+```
+{x: 10.435175428044776, y: 9.584035371572032, z: 2.3440549911816726}
+{x: 10.435175428044776, y: 9.584035371572032, z: 2.3440549911816726}
+```
+
+But the (absolut) maxima of the rescaled version with the factor is reduced and all of them are below 1:
+```python
+print(Roessler_rescaled.maxima)
+print(Roessler_rescaled_2.maxima)
+```
+
+```
+{x: 1.000052590888878,  y: 0.9991044611593582, z: 0.9997609510430012}
+{x: 0.9901979740914344, y: 0.9916789571237319, z: 0.989455868543799}
+```
+
+
+A further approach is to increase the precision of the solve_ivp function by providing kwargs as in the documentation.
+The default RK45 method can be adapted, but here the provision of max_step increases the precision (and calculation time).
+For details see: https://docs.scipy.org/doc/scipy/reference/generated/scipy.integrate.solve_ivp.html
+
+```python
+kwargs_solve_ivp = {'method' : 'RK45', 'max_step' : 0.01}
+Roessler_3 = DEQscaler(args_solve_ivp_ini_val, t, dydt, f_t_y, diff_eq_parameters, kwargs_solve_ivp=kwargs_solve_ivp)
+```
+
+Note that the kwargs are also passed on to the new Roessler_rescaled_3 instance.
+```python
+Roessler_rescaled_3 = DEQscaler(*Roessler_3.create_rescaled_Diff_Eq())
+Roessler_rescaled_3.determine_max()
+```
+
+The issue may be smaller (in some cases) than before but certainly still prevails:
+```python
+print(Roessler_rescaled.maxima)
+print(Roessler_rescaled_3.maxima)
+```
+
+```
+{x: 1.000052590888878,  y: 0.9991044611593582, z: 0.9997609510430012}
+{x: 1.0000144356472667, y: 1.0000098414715495, z: 1.000000000000356}
+```
+
+A max_scale_factor is hence advisible.
+
+Of course these can also be combined.
+```python
+Roessler_4 = DEQscaler(args_solve_ivp_ini_val, t, dydt, f_t_y, diff_eq_parameters, max_scale_factor=1.01, kwargs_solve_ivp=kwargs_solve_ivp)
+Roessler_rescaled_4 = DEQscaler(*Roessler_4.create_rescaled_Diff_Eq())
+Roessler_rescaled_4.determine_max()
+```
+
+to improve upon the initial rescaling results.
+```python
+print(Roessler_rescaled.maxima)
+print(Roessler_rescaled_2.maxima)
+print(Roessler_rescaled_4.maxima)
+```
+
+```
+{x: 1.000052590888878,  y: 0.9991044611593582, z: 0.9997609510430012}
+{x: 0.9901979740914344, y: 0.9916789571237319, z: 0.989455868543799}
+{x: 0.99011297853811,   y: 0.9901086672002818, z: 0.9900990099013388}
+```
+
+
+A manual choice of maxima for rescaling is also possible:
+```python
+import copy
+
+print(Roessler.maxima)
+manual_maxima = copy.deepcopy(Roessler.maxima)
+
+manual_maxima[x] = 12
+manual_maxima[y] = 11
+manual_maxima[z] = 2.7
+
+"""
+Alternatively with same effect:
+man_max = [12, 11, 23]
+manual_maxima = {ele[0] : man_max[pos] for pos, ele in enumerate(manual_maxima.items())}
+"""
+
+properties_rescaled_5 = Roessler.create_rescaled_Diff_Eq(maxima_items=manual_maxima)
+Roessler_rescaled_5 = DEQscaler(*properties_rescaled_5)
+
+Roessler_rescaled_5.determine_max()
+print(Roessler_rescaled_5.maxima)
+```
+
+```
+{x: 10.435175428044776, y: 9.584035371572032,  z: 2.3440549911816726}
+{x: 0.8696117872807255, y: 0.8632412602527475, z: 0.8683443670516}
+```
+
+It is also possible to only calculate the numerical solution.
+However this function will also be called by .determine_max() which in turn is called by .create_rescaled_Diff_Eq() unless maxima_items is provided.
+Hence it is advisible to either call .determine_max() [in case of a rescaled system] or .create_rescaled_Diff_Eq() since computationally there is little difference.
 
 # Funding
-<img src="pictures/hmdis_logo.jpg" alt="HMDIS-Logo" width="300"/>
+<img src="images/hmdis_logo.jpg" alt="HMDIS-Logo" width="300"/>
--- source/DEQscaler.py
+++ source/DEQscaler.py
@@ -1,1 +1,189 @@
 # DEQscaler
+"""
+Tool to rescale systems of ordinary differential equations of the type dy /dt = f(t,y) with initial conditions y(t0)=y0 and parameters.
+1. A DEQ is formulated in a sympy representation.
+2. The sympy representation is used to apply the parameters and thence create f(t,y) only.
+3. The equation dy/dt=f(t,y) is solved numerically according to given precision and method by scipy.integrate.solve_ivp
+4. The absolut maximum values of the numerical solution are used to rescale the problem to the domain [-1,1] (analog computer implementation).
+"""
+
+import numpy as np
+import sympy
+from scipy import integrate
+
+"""
+A class to represent and rescale a system of ordinary differential equations of the type dy/dt = f(t,y) with initial conditions y(t0)=y0 and parameters.
+The rescaling is done by determining the maxima of the trajectory components of y using the solve_ivp function from scipy.
+
+args_solve_ivp_ini_val: List of essential arguments t_span and y0 for scipy.integrate.solve_ivp. Cf. for further detail: https://docs.scipy.org/doc/scipy/reference/generated/scipy.integrate.solve_ivp.html
+t:                      sympy.Symbol('t') the time variable.
+dydt:                   List of sympy symbols representing the components of y. 
+f_t_y:                  List of sympy expressions representing f(t,y) including the sympy symbols representing the parameters. Note that t may not be part of f(t,y) at this time.
+diff_eq_parameters:     Dictionary with the parameter sympy symbols as keys and their values.
+max_scale_factor:       Optional factor to adapt the maxima used for rescaling.
+kwargs_solve_ivp:       Optional arguments that can be passed to scipy.integrate.solve_ivp. Cf. link above for details.    
+"""
+class DEQscaler:
+
+    def __init__(self, args_solve_ivp_ini_val, t, dydt, f_t_y, diff_eq_parameters, max_scale_factor=1.0,
+                 kwargs_solve_ivp={}):
+        self.args_solve_ivp_ini_val = args_solve_ivp_ini_val
+        self.t = t
+        self.dydt = dydt
+        self.f_t_y = f_t_y
+        self.diff_eq_parameters = diff_eq_parameters
+        self.max_scale_factor = max_scale_factor
+        self.kwargs_solve_ivp = kwargs_solve_ivp
+
+        self.num_f_t_y = []
+        self.num_sol = []
+        self.maxima = {}
+
+    """
+    Display the properties of the created object as text in the console.
+    """
+    def show_eqn(self):
+        print("\nInitial values: Starting value y(t0)=y0; integrate numerically wrt. t until tf is reached.")
+        print("y0       = " + str(self.args_solve_ivp_ini_val[1]))
+        print("(t0, tf) = " + str(self.args_solve_ivp_ini_val[0]))
+        if self.max_scale_factor != 1.0:
+            print("max_scale_factor = " + str(self.max_scale_factor))
+
+        if self.kwargs_solve_ivp != {}:
+            print("\nAdditional kwargs passed to scipy.integrate.solve_ivp:")
+            print(self.kwargs_solve_ivp)
+
+        print("")
+
+        print("Parameters:")
+        for label, val in self.diff_eq_parameters.items():
+            print("{:} = {:}".format(str(label), val))
+
+        print("")
+
+        print("System of Equations: [Note the order is alphabetical in the sympy-symbols present.]")
+        for pos, ele in enumerate(self.dydt):
+            print("{:} = {:}".format(str(ele), str(self.f_t_y[pos])))
+
+        print("")
+
+        print("System of Equations (for numerical calculation):")
+        """Create tuple to substitute parameters with their values."""
+        substitution_parameters = [(ele[0], ele[1]) for ele in self.diff_eq_parameters.items()]
+        """Use the substitution to display the numerical values."""
+        for pos, ele in enumerate(self.dydt):
+            print("{:} = {:}".format(str(ele), str(self.f_t_y[pos].subs(substitution_parameters))))
+
+    """
+    Use sympy.lambdify to create numeric scipy functions with substituted values for the parameters.
+    """
+    def create_num_f_t_y(self):
+
+        """
+        Create tuple of the variables used to solve later on.
+        """
+        calc_variables = tuple([self.t] + self.dydt)
+
+        """
+        Iterate over the f(t,y) functions, i.e. proceed elementwise.
+        For every function use the .subs() to replace the parameters with their numeric values.        
+        Use the tuple calc_variables to specify on which variables the numeric functions depend.        
+        Ensure a scipy object is created using the modules kwarg.
+        """
+        self.num_f_t_y = [
+            sympy.lambdify([calc_variables], ele.subs(self.diff_eq_parameters), modules=["scipy", "numpy"]) for ele in
+            self.f_t_y]
+
+    """
+    The scipy.integrate.solve_ivp requires a callable f(t,y) function fun.
+    This is implemented here using the num_f_t_y.
+    """
+    def derivative(self, t, state):
+        """
+        Create List of step values including t. [The t might not be used according to the specific equation, but is required for solve_ivp.]
+        """
+        var_step_vals = np.concatenate(([t], state))
+
+        """
+        Evaluate num_f_ty using the step values.
+        """
+        calc_eqns = [ele(var_step_vals) for ele in self.num_f_t_y]
+
+        return calc_eqns
+
+    """
+    Perform the numeric integration to solve the system using the args and potential kwargs specified.
+    """
+    def solve_numerically(self):
+        """
+        Create the lambdify numeric functions to be used
+        """
+        self.create_num_f_t_y()
+
+        """
+        Use the scipy.integrate.solve_ivp function.
+        """
+        self.num_sol = integrate.solve_ivp(self.derivative, *self.args_solve_ivp_ini_val, **self.kwargs_solve_ivp)
+
+    """
+    Determine the absolut maxima of the trajectories using the numerical solution.
+    """
+    def determine_max(self):
+        """
+        Calculate the numerical solution.
+        """
+        self.solve_numerically()
+
+        """
+        Determine the absolut maxima.
+        Potential adaption: Use the numpy ceiling function np.ceil to round to 3 significant figures.
+        by replacint "np.absolute(ele).max()" with "np.ceil(np.absolute(ele).max()*1000)/1000"
+        """
+        self.maxima = {self.dydt[pos]: np.absolute(ele).max() for pos, ele in enumerate(self.num_sol.y)}
+
+    """
+    Create arguments to instanciate the new, rescaled DEQ system 
+    """
+    def create_rescaled_Diff_Eq(self, maxima_items=None):
+        """
+        If no individual maxima are provided calculate maxima by solving numerically and use these.
+        """
+        if maxima_items is None:
+            self.determine_max()
+            maxima_items = self.maxima
+
+        """
+        Make maxima_items iterable.
+        """
+        maxima_items = maxima_items.items()
+
+        """
+        Adapt the maxima by multipling with the max_scale_factor.
+        """
+        round_up_maxima = {key: value * self.max_scale_factor for key, value in maxima_items}
+
+        """
+        Rescale the initial values, i.e. y0 in the args_solve_ivp_ini_val
+        """
+        rescaled_args_solve_ivp_ini_val = [self.args_solve_ivp_ini_val[0],
+                                           [self.args_solve_ivp_ini_val[1][pos] * 1 / round_up_maxima[ele] for pos, ele
+                                            in enumerate(self.dydt)]]
+
+        """
+        Create the tuples used to substitute the symbolic variables with the product of the rescaling factor and the symbolic variable.
+        """
+        substitution_list_tuples = [(ele[0], ele[0] * ele[1]) for ele in round_up_maxima.items()]
+
+        """
+        Create the factor list used to scale each expression in f_t_y as a whole.
+        """
+        rescale_eqns_factors = [1 / ele[1] for ele in round_up_maxima.items()]
+
+        """
+        Create the rescaled equation rescaled_f_t_y.
+        """
+        rescaled_f_t_y = [rescale_eqns_factors[pos] * ele.subs(substitution_list_tuples) for pos, ele in
+                          enumerate(self.f_t_y)]
+
+        return [rescaled_args_solve_ivp_ini_val, self.t, self.dydt, rescaled_f_t_y, self.diff_eq_parameters,
+                self.max_scale_factor, self.kwargs_solve_ivp]
